0761f0376de2bd05e3e3ce844af49d4b82eb2890
mimirandi21 <mimirandi21@gmail.com>
2021-06-11 10:43:27 -0500

    work on sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,23 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+function bubbleSort(arr) {}
 
-}
+function selectionSort(arr) {}
 
-function selectionSort(arr) {
-
-}
-
-function mergeSort(arr) {
-
-}
+function mergeSort(arr) {}
 
 function merge(left, right) {
+	let results = [];
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
 
+	return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
