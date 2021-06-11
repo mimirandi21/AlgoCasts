f031aac6b56cfe2d9dda0f0f0535182e005b7d0b
mimirandi21 <mimirandi21@gmail.com>
2021-06-11 10:54:26 -0500

    continue work on sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -5,7 +5,15 @@ function bubbleSort(arr) {}
 
 function selectionSort(arr) {}
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+	if (arr.length <= 1) return arr;
+
+	const center = Math.floor(arr.length / 2);
+	const left = arr.slice(0, center);
+	const right = arr.slice(center);
+
+	return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {
 	let results = [];
